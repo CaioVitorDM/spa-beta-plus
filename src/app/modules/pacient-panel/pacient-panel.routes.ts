7e1abcb3e78900f126f4196dcbc136763a798145
wisla.argolo.133 <wisla.argolo.133@ufrn.edu.br>
2024-05-01 22:08:57 -0300

    feat: adicionando tela de medic para paciente

--- src/app/modules/pacient-panel/pacient-panel.routes.ts
+++ src/app/modules/pacient-panel/pacient-panel.routes.ts
@@ -2,10 +2,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {PacientPanelComponent} from './pacient-panel.component';
 import {PatientDashboardComponent} from './pages/patient-dashboard/patient-dashboard.component';
+import {MedicComponent} from './pages/medic/medic.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: PatientDashboardComponent},
+  {path: 'medic', component: MedicComponent},
 ];
 
 @NgModule({
