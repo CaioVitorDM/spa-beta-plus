e850790b5fa060dd4285a93e1d5f05c25a5c8255
gabriel-ribeiro-099 <gabriel.ribeiro.099@ufrn.edu.br>
2024-05-21 15:55:32 -0300

    FrontBetaPacienteAjuste

--- src/app/modules/pacient-panel/pages/beta-graph/beta-general/beta-general.component.ts
+++ src/app/modules/pacient-panel/pages/beta-graph/beta-general/beta-general.component.ts
@@ -20,6 +20,7 @@ import { PatientList } from 'src/app/models/User';
 })
 export class BetaGeneralComponent implements OnInit {
   @Input() dataSource: PatientList[] = [];
+  sortOrder: 'asc' | 'desc' = 'desc'; // Estado da ordenação
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -32,28 +33,49 @@ export class BetaGeneralComponent implements OnInit {
       {
         id: 1,
         createdAt: '2023-05-20T10:00:00Z',
-        name: 'John Doe',
-        birthDate: '1990-05-15',
+        name: '500',
+        birthDate: '2024-04-05',
         phoneNumber: '123-456-7890',
         email: 'john.doe@example.com'
       },
       {
         id: 2,
         createdAt: '2023-05-21T10:00:00Z',
-        name: 'Jane Smith',
-        birthDate: '1985-08-30',
+        name: '1000',
+        birthDate: '2024-03-05',
         phoneNumber: '987-654-3210',
         email: 'jane.smith@example.com'
       },
       {
         id: 3,
         createdAt: '2023-05-22T10:00:00Z',
-        name: 'Alice Johnson',
-        birthDate: '1975-02-10',
+        name: '1500',
+        birthDate: '2024-05-05',
         phoneNumber: '555-555-5555',
         email: 'alice.johnson@example.com'
       }
     ];
+
+    // Ordena por padrão de forma decrescente
+    this.sortByBirthDate();
+  }
+
+  sortByBirthDate(): void {
+    this.dataSource.sort((a, b) => {
+      const dateA = new Date(a.birthDate).getTime();
+      const dateB = new Date(b.birthDate).getTime();
+
+      if (this.sortOrder === 'asc') {
+        return dateA - dateB;
+      } else {
+        return dateB - dateA;
+      }
+    });
+  }
+
+  toggleSortOrder(): void {
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+    this.sortByBirthDate();
   }
 
   printButton(action: string, event?: MouseEvent): void {
